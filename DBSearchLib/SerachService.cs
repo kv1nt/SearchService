a7840a943bad11afbc7dc301fbc8043fe5c37800
Vitalii Palamarchuk <vitalii.palamarchuk@onseo.biz>
2020-08-21 13:48:47 +0300

    Search Service Init

---
+++ DBSearchLib/SerachService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+using System.Text;
+
+namespace DBSearchLib
+{
+    public class SerachService
+    {
+        /// <summary>
+        /// Search in Db
+        /// </summary>
+        /// <typeparam name="T">Model Data Transter</typeparam>
+        /// <param name="ps">Model Data Transter</param>
+        /// <param name="tableName">Database Table Name</param>
+        /// <param name="connectionString">Database Connection string</param>
+        public void Search<T>(T ps, string tableName, string connectionString) where T : class
+        {
+            Type tModelType = ps.GetType();
+            PropertyInfo[] propInfos1 = tModelType.GetProperties();
+            var props = propInfos1;
+            var selectQuery = new StringBuilder($"SELECT * FROM {tableName}");
+            selectQuery.Append($" WHERE");
+            string typeVariant = string.Empty;
+            var val = string.Empty;
+            decimal res = 0;
+            foreach (var item in props)
+            {
+                if(item.PropertyType.Name == "Decimal")
+                {
+                  
+                }
+                if (item.GetValue(ps) != null)
+                {
+                    typeVariant = item.PropertyType.Name == "String" ? $" [{item.Name}] = '{item.GetValue(ps)}' AND" :
+                                $" [{item.Name}] = {item.GetValue(ps)} AND";
+                    selectQuery.Append(typeVariant);
+                }
+
+            }
+            selectQuery.Remove(selectQuery.Length - 3, 3);
+            Console.Write(selectQuery);
+            using (var conn = new SqlConnection(connectionString))
+                try
+                {
+                    conn.Open();
+                    SqlCommand command = new SqlCommand();
+                    command.CommandText = selectQuery.ToString();
+                    command.Connection = conn;
+                    SqlDataAdapter adapter = new SqlDataAdapter(selectQuery.ToString(), conn);
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds);
+                    SqlDataReader reader = command.ExecuteReader();
+                    //adapter.Fill(ds);
+
+                    while (reader.Read())
+                    {
+
+                        foreach (var item in props)
+                        {
+                            var name = item.Name;
+                            var val = reader[item.Name].ToString();
+                        }
+                    }
+
+
+
+                }
+                catch (SqlException ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+        }
+    }
+}
---
+++ TestSearchService/Program.cs
@@ -0,0 +1,37 @@
+﻿using DBSearchLib;
+using System;
+
+namespace TestSearchService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var search = new SerachService();
+
+            search.Search(new Product{ Name = "string", Price = 1 }, 
+                "Product", @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ASPNetDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Product
+    {
+    
+        public  Guid? Id { get; set; }
+        public string PhotoId { get; set; }
+     
+        public  string Name { get; set; }
+
+        public  string Type { get; set; }
+
+        public  string Color { get; set; }
+
+        public  decimal Weight { get; set; }
+ 
+        public  decimal Price { get; set; }
+        public string Description { get; set; }
+        public int MyPropertyTestInt { get; set; }
+    }
+}
